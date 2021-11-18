b5f949a056c1b006ea3746e01ed1081776b6f43a
Anastasija Voropajeva <a.voropajeva3@gmail.com>
2021-11-18 23:48:42 +0200

    changing mult_table...

---
+++ exercises/mult_table/main.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func MultTable() (s [10][10]int) {
+	for i := range s {
+		for j := range s[i] {
+			s[i][j] = i * j
+		}
+	}
+	return s
+}
+
+func main() {
+	table := MultTable()
+	for i := range table {
+		for j := range table[i] {
+			fmt.Printf("%3d", table[i][j])
+		}
+		fmt.Println()
+	}
+}
