555c43397bac753ca336789f3f89b29921305bdf
Barash456 <denis.barannik@gmail.com>
2022-01-22 16:19:10 +0200

    fixed test

--- exercises/filter_odds_and_evens/filteroddsandevens_test.go
+++ exercises/filter_odds_and_evens/filteroddsandevens_test.go
@@ -1,18 +1,18 @@
-package main 
+package main
 
 import "testing"
 
-func TestFilterEvens(t * testing.T) {
+func TestFilterEvens(t *testing.T) {
 	for _, tc := range []struct {
 		want []int
-		s []int
+		s    []int
 	}{
 		{[]int{}, []int{}},
 		{[]int{1}, []int{1}},
 		{[]int{2}, []int{2}},
-		{[]int{3,6,9}, []int{3,6,9}},
-		{[]int{6,4,2}, []int{6,4,2}},
-	}{
+		{[]int{3, 6, 9}, []int{3, 6, 9}},
+		{[]int{6, 4, 2}, []int{6, 4, 2}},
+	} {
 		got := FilterEvens(tc.s)
 		if !equal(got, tc.want) {
 			t.Errorf("FilterEvens(%v) = %v, want = %v", tc.s, got, tc.want)
@@ -20,8 +20,14 @@ func TestFilterEvens(t * testing.T) {
 	}
 }
 
-func equal (t, y []int) bool 
-
-func TestFiterOdds(t * testing.T){
-	
+func equal(a, b []int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for i := range a {
+		if a[i] != b[i] {
+			return false
+		}
+	}
+	return true
 }
